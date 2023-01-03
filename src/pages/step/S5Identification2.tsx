c7e971371f360eb8b62f9d4aade171cb96965195
tofreshmilk <ddully89@gmail.com>
2023-01-03 14:56:50 +0900

    props로 key를 대체전

--- src/pages/step/S5Identification2.tsx
+++ src/pages/step/S5Identification2.tsx
@@ -1,21 +1,7 @@
 import axios from 'axios';
-import { useEffect, useState } from 'react';
 
 const S5Identification2 = (_props: any) => {
-  const [key, setKey] = useState({
-    userName: '',
-    userPhone: '',
-    userBirth: '',
-    userHash: '',
-    authHash: '',
-    mTxId: ``,
-    reqSvcCd: _props.k.reqSvcCd,
-    apiKey: _props.k.apiKey,
-    mid: _props.k.mid,
-  });
-  const handleInputChange = (e: any) => {
-    setKey({ ...key, [e.target.name]: e.target.value });
-  };
+  const { k } = _props;
   const onSubmit = () => {
     const width = 400;
     const height = 620;
@@ -27,24 +13,12 @@ const S5Identification2 = (_props: any) => {
     );
     const url = `${process.env.NEXT_PUBLIC_API_URL}/beforeIdentification`;
     const data2 = {
-      mTxId: key.mTxId,
-      userName: key.userName,
-      userPhone: key.userPhone,
+      mTxId: k.mTxId,
+      userName: k.userName,
+      userPhone: k.userPhone,
     };
     axios.post(url, data2);
   };
-  useEffect(() => {
-    const { k } = _props;
-    setKey({
-      ...key,
-      userName: k.userName,
-      userPhone: k.userPhone,
-      userBirth: k.userBirth,
-      userHash: k.userHash,
-      authHash: k.authHash,
-      mTxId: k.mTxId,
-    });
-  }, [_props]);
   return (
     <form
       name="saForm"
@@ -53,19 +27,14 @@ const S5Identification2 = (_props: any) => {
       action="https://sa.inicis.com/auth"
       onSubmit={onSubmit}
     >
-      <input
-        type="hidden"
-        name="mid"
-        value={key.mid}
-        onChange={handleInputChange}
-      />
-      <input type="hidden" name="reqSvcCd" defaultValue="01" />
-      <input
-        type="hidden"
-        name="mTxId"
-        value={key.mTxId}
-        onChange={handleInputChange}
-      />
+      <input type="hidden" name="flgFixedUser" defaultValue="Y" />
+      <input type="hidden" name="mid" defaultValue={k.mid} />
+      <input type="hidden" name="reqSvcCd" defaultValue={k.reqSvcCd} />
+      <input type="hidden" name="mTxId" defaultValue={k.mTxId} />
+      <input type="hidden" name="authHash" defaultValue={k.authHash} />
+      <input type="hidden" name="userName" defaultValue={k.userName} />
+      <input type="hidden" name="userPhone" defaultValue={k.userPhone} />
+      <input type="hidden" name="userHash" defaultValue={k.userHash} />
       <input
         type="hidden"
         name="successUrl"
@@ -75,37 +44,6 @@ const S5Identification2 = (_props: any) => {
         type="hidden"
         name="failUrl"
         defaultValue="https://join.smartelmobile.com/api/receiveInicisFail"
-      />
-      <input
-        type="hidden"
-        name="authHash"
-        value={key.authHash}
-        onChange={handleInputChange}
-      />
-      <input type="hidden" name="flgFixedUser" defaultValue="Y" />
-      <input
-        type="hidden"
-        name="userName"
-        value={key.userName}
-        onChange={handleInputChange}
-      />
-      <input
-        type="hidden"
-        name="userPhone"
-        value={key.userPhone}
-        onChange={handleInputChange}
-      />
-      <input
-        type="hidden"
-        name="userBirth"
-        value={key.userBirth}
-        onChange={handleInputChange}
-      />
-      <input
-        type="hidden"
-        name="userHash"
-        value={key.userHash}
-        onChange={handleInputChange}
       />
       <input type="hidden" name="reservedMsg" defaultValue="isUseToken=Y" />
       <button
