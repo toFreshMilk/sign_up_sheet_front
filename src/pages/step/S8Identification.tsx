5ae5b3732b9a28220c592127aab657b54ffd246c
nojam <a>
2023-02-10 15:03:11 +0900

    툴팁 좀 안맞음

--- src/pages/step/S8Identification.tsx
+++ src/pages/step/S8Identification.tsx
@@ -1,5 +1,7 @@
+import 'css-tooltip/dist/css-tooltip.css';
+
 import { useRouter } from 'next/router';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import styles from '@/styles/utils.module.css';
 import { Main } from '@/templates/Main';
@@ -31,7 +33,6 @@ const S8Identification = () => {
   const [monthYear, setMonthYear] = useState('');
   const [identificationType, setIdentificationType] =
     useState(identificationTypes);
-  const svg = useRef(null);
   useEffect(() => {
     const S5PersonalInfo = sessionStorage.getItem('S5PersonalInfo') || '';
     const S5PersonalInfoJson = JSON.parse(S5PersonalInfo);
@@ -42,7 +43,8 @@ const S8Identification = () => {
     <Main>
       <div className={``}>
         <h2 className={`${styles.stepTitle}`}>
-          {s5PersonalInfo?.userName || '고객'}님의 신분증 <br /> 정보를 입력해주세요
+          {s5PersonalInfo?.userName || '고객'}님의 신분증 <br /> 정보를
+          입력해주세요
         </h2>
         <h3 className="text-[16px] text-[#868e96] mt-[8px]">
           부정가입방지를 위한 과정이에요
@@ -125,36 +127,21 @@ const S8Identification = () => {
         <br className={'mt-[32px]'} />
         <div className={`${styles.usimSubTitle} flex justify-items-center`}>
           <div className={'mr-[4px]'}>발급일자</div>
-          <div
-            ref={svg}
-            onMouseOver={(e) => {
-              // console.log(svg.current);
-              // const { offsetTop, offsetLeft, offsetBottom, offsetRight } = svg.current as any;
-              // console.log(offsetTop, offsetLeft, offsetBottom, offsetRight);
-              console.log(e.pageX, e.pageY);
-            }}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              className="text-[#dee1e7] w-[20px] h-[20px]"
-            >
-              <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M10 18C14.4183 18 18 14.4183 18 10C18 5.58172 14.4183 2 10 2C5.58172 2 2 5.58172 2 10C2 14.4183 5.58172 18 10 18ZM8.91699 12.0635V11.9238C8.92334 10.3433 9.34863 9.86084 10.1104 9.38477C10.6562 9.0293 11.0815 8.63574 11.0752 8.05176C11.0815 7.41699 10.5864 7.00439 9.9707 6.99805C9.38037 7.00439 8.82812 7.39795 8.80273 8.14062H7C7.03174 6.35693 8.3584 5.5 9.9834 5.5C11.7544 5.5 13.0049 6.42041 13.0049 7.98828C13.0049 9.03564 12.4653 9.6958 11.6338 10.1973C10.9165 10.6226 10.5991 11.0352 10.5928 11.9238V12.0635H8.91699ZM9.79297 14.9326C9.19629 14.9326 8.71387 14.4565 8.72656 13.8662C8.71387 13.2822 9.19629 12.8062 9.79297 12.8125C10.3579 12.8062 10.8467 13.2822 10.8467 13.8662C10.8467 14.4565 10.3579 14.9326 9.79297 14.9326Z"></path>
-            </svg>
-            <span
-              role="tooltip"
-              className={`${styles.tooltip}`}
-              data-popper-reference-hidden="false"
-              data-popper-escaped="false"
-              data-popper-placement="top"
-            >
-              운전면허증 앞면 하단의 날짜를 적어주세요
-              <span className={`${styles.tooltipArrow}`}></span>
-            </span>
+          <div>
+            <a href="#" data-tooltip="주민등록증 앞면 하단의 날짜를 적어주세요">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="text-[#dee1e7] w-[20px] h-[20px]"
+              >
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M10 18C14.4183 18 18 14.4183 18 10C18 5.58172 14.4183 2 10 2C5.58172 2 2 5.58172 2 10C2 14.4183 5.58172 18 10 18ZM8.91699 12.0635V11.9238C8.92334 10.3433 9.34863 9.86084 10.1104 9.38477C10.6562 9.0293 11.0815 8.63574 11.0752 8.05176C11.0815 7.41699 10.5864 7.00439 9.9707 6.99805C9.38037 7.00439 8.82812 7.39795 8.80273 8.14062H7C7.03174 6.35693 8.3584 5.5 9.9834 5.5C11.7544 5.5 13.0049 6.42041 13.0049 7.98828C13.0049 9.03564 12.4653 9.6958 11.6338 10.1973C10.9165 10.6226 10.5991 11.0352 10.5928 11.9238V12.0635H8.91699ZM9.79297 14.9326C9.19629 14.9326 8.71387 14.4565 8.72656 13.8662C8.71387 13.2822 9.19629 12.8062 9.79297 12.8125C10.3579 12.8062 10.8467 13.2822 10.8467 13.8662C10.8467 14.4565 10.3579 14.9326 9.79297 14.9326Z"
+                ></path>
+              </svg>
+            </a>
           </div>
         </div>
         <input
